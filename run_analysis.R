e0d10d3cf47b5283be9dd9c2367828f5907324c7
alisadunk <at.pobox@gmail.com>
2017-05-22 16:07:36 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -11,13 +11,43 @@ activities <- c('WALKING','WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 'SITTING', '
 #6 LAYING
 
 
-labels = 1:6
-names(labels) <- activities
+setwd('../../FProject/')
+#read files
+x<-read.table("UCI HAR Dataset/features.txt")
+a<-read.table("UCI HAR Dataset/activity_labels.txt")
 
-#subject - person taking part in the study
+#measure std...
 
-setwd('Data Science with R/FProject/UCI HAR Dataset/')
-result <- read.fwf(file = 'test/X_test.txt', widths = rep(16,30) , header=FALSE)
-write.table(result, file = 'test/X_result.txt', row.names = FALSE)
-file.show('test\X_result.txt')
+std_x<-grep(".*mean.*|.*std.*",x[,2])
+std<-x[std_x,2]
+std=gsub('-mean','-Mean',std)
+std=gsub('-std','Std',std)
+std<-gsub('[-()]','',std)
+
+#load data
+test<-read.table("UCI HAR Dataset/test/X_test.txt")[std_x]
+test_activities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+tsub <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(tsub, test_activities, test)
+train<-read.table("UCI HAR Dataset/train/X_train.txt")[std_x]
+ta<-read.table("UCI HAR Dataset/train/Y_train.txt")
+ts<-read.table("UCI HAR dataset/train/subject_train.txt")
+train <- cbind(ts, ta, train)
+
+
+#Merge 
+ad <- rbind(train, test)
+colnames(ad) <- c("subject", "activity", std)
+
+#assign correct values
+ad$activity <- factor(ad$activity
+                      , levels = a[,1]
+                      , labels = a[,2])
+ad$subject <- as.factor(ad$subject)
+
+#result output
+write.table(ad
+            , "UCI HAR Dataset/test/tidy_result.txt"
+            , row.names = FALSE
+            , quote = FALSE)
 
