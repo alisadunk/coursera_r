ec66d069bbfad950510c92c2b14bbc237c43bff9
alisadunk <at.pobox@gmail.com>
2017-05-22 15:32:56 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,19 @@
 #code for the run
+##analysis of FitBit Data
+##Activities: 
+activities <- c('WALKING','WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 'SITTING', 'STANDING', 'LAYING')
+##Activity Labels (activity_lables.txt)
+#1 WALKING
+#2 WALKING_UPSTAIRS
+#3 WALKING_DOWNSTAIRS
+#4 SITTING
+#5 STANDING
+#6 LAYING
+
+
+labels = 1:6
+names(labels) <- activities
+
+#subject - person taking part in the study
+
+measurement
